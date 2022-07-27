eae45d877348d173bdcc3437f1d96ff5fcfb26f6
yad <459647480@qq.com>
2022-07-27 19:33:34 +0800

    🎨 test data in common file
    Signed-off-by: yad <459647480@qq.com>

--- test/integration/async_server.ts
+++ test/integration/async_server.ts
@@ -6,64 +6,14 @@ import {get, isEmpty} from 'lodash';
 import * as shell from 'shelljs';
 import * as MQTT from 'aedes';
 
-import {OpenFunctionContext} from '../../src/openfunction/function_context';
+// import {OpenFunctionContext} from '../../src/openfunction/function_context';
 import getAysncServer from '../../src/openfunction/async_server';
 
-const TEST_CONTEXT: OpenFunctionContext = {
-  name: 'test-context',
-  version: '1.0.0',
-  runtime: 'Async',
-  port: '8080',
-  inputs: {
-    cron: {
-      uri: 'cron_input',
-      componentName: 'binding-cron',
-      componentType: 'bindings.cron',
-    },
-    mqtt_binding: {
-      uri: 'default',
-      componentName: 'binding-mqtt',
-      componentType: 'bindings.mqtt',
-    },
-    mqtt_sub: {
-      uri: 'webup',
-      componentName: 'pubsub-mqtt',
-      componentType: 'pubsub.mqtt',
-    },
-  },
-  outputs: {
-    cron: {
-      uri: 'cron_output',
-      operation: 'delete',
-      componentName: 'binding-cron',
-      componentType: 'bindings.cron',
-    },
-    localfs: {
-      uri: 'localstorage',
-      operation: 'create',
-      componentName: 'binding-localfs',
-      componentType: 'bindings.localstorage',
-      metadata: {
-        fileName: 'output-file.txt',
-      },
-    },
-    mqtt_pub: {
-      uri: 'webup_pub',
-      componentName: 'pubsub-mqtt',
-      componentType: 'pubsub.mqtt',
-    },
-  },
-};
-
-const TEST_PAYLOAD = {data: 'hello world'};
-const TEST_CLOUD_EVENT = {
-  specversion: '1.0',
-  id: 'test-1234-1234',
-  type: 'ce.openfunction',
-  source: 'https://github.com/OpenFunction/functions-framework-nodejs',
-  traceparent: '00-65088630f09e0a5359677a7429456db7-97f23477fb2bf5ec-01',
-  data: TEST_PAYLOAD,
-};
+import {
+  TEST_CLOUD_EVENT,
+  TEST_CONTEXT,
+  TEST_PAYLOAD,
+} from '../data/test_data/async_plugin';
 
 describe('OpenFunction - Async - Binding', () => {
   const APPID = 'async.dapr';
--- test/integration/async_server_plugin.ts
+++ test/integration/async_server_plugin.ts
@@ -6,120 +6,17 @@ import {get, isEmpty} from 'lodash';
 import * as shell from 'shelljs';
 import * as MQTT from 'aedes';
 
-import {OpenFunctionContext} from '../../src/openfunction/function_context';
+// import {OpenFunctionContext} from '../../src/openfunction/function_context';
 import getAysncServer from '../../src/openfunction/async_server';
 import {getUserPlugins} from '../../src/loader';
-import {FrameworkOptions} from '../../src/options';
+// import {FrameworkOptions} from '../../src/options';
 import assert = require('assert');
-
-const TEST_CONTEXT: OpenFunctionContext = {
-  name: 'test-context',
-  version: '1.0.0',
-  runtime: 'Async',
-  port: '8080',
-  inputs: {
-    cron: {
-      uri: 'cron_input',
-      componentName: 'binding-cron',
-      componentType: 'bindings.cron',
-    },
-    mqtt_binding: {
-      uri: 'default',
-      componentName: 'binding-mqtt',
-      componentType: 'bindings.mqtt',
-    },
-    mqtt_sub: {
-      uri: 'webup',
-      componentName: 'pubsub-mqtt',
-      componentType: 'pubsub.mqtt',
-    },
-  },
-  outputs: {
-    cron: {
-      uri: 'cron_output',
-      operation: 'delete',
-      componentName: 'binding-cron',
-      componentType: 'bindings.cron',
-    },
-    localfs: {
-      uri: 'localstorage',
-      operation: 'create',
-      componentName: 'binding-localfs',
-      componentType: 'bindings.localstorage',
-      metadata: {
-        fileName: 'output-file.txt',
-      },
-    },
-    mqtt_pub: {
-      uri: 'webup_pub',
-      componentName: 'pubsub-mqtt',
-      componentType: 'pubsub.mqtt',
-    },
-  },
-};
-const TEST_PLUGIN_OPTIONS: FrameworkOptions = {
-  port: '',
-  target: '',
-  sourceLocation: process.cwd() + '/test/data',
-  signatureType: 'event',
-  printHelp: false,
-  context: {
-    name: 'test-context-plugin',
-    version: '1.0.0',
-    runtime: 'Async',
-    port: '8080',
-    inputs: {
-      cron: {
-        uri: 'cron_input',
-        componentName: 'binding-cron',
-        componentType: 'bindings.cron',
-      },
-      mqtt_binding: {
-        uri: 'default',
-        componentName: 'binding-mqtt',
-        componentType: 'bindings.mqtt',
-      },
-      mqtt_sub: {
-        uri: 'webup',
-        componentName: 'pubsub-mqtt',
-        componentType: 'pubsub.mqtt',
-      },
-    },
-    outputs: {
-      cron: {
-        uri: 'cron_output',
-        operation: 'delete',
-        componentName: 'binding-cron',
-        componentType: 'bindings.cron',
-      },
-      localfs: {
-        uri: 'localstorage',
-        operation: 'create',
-        componentName: 'binding-localfs',
-        componentType: 'bindings.localstorage',
-        metadata: {
-          fileName: 'output-file.txt',
-        },
-      },
-      mqtt_pub: {
-        uri: 'webup_pub',
-        componentName: 'pubsub-mqtt',
-        componentType: 'pubsub.mqtt',
-      },
-    },
-    prePlugins: ['demo-plugin'],
-    postPlugins: ['demo-plugin'],
-  },
-};
-const TEST_PAYLOAD = {data: 'hello world'};
-const TEST_CLOUD_EVENT = {
-  specversion: '1.0',
-  id: 'test-1234-1234',
-  type: 'ce.openfunction',
-  source: 'https://github.com/OpenFunction/functions-framework-nodejs',
-  traceparent: '00-65088630f09e0a5359677a7429456db7-97f23477fb2bf5ec-01',
-  data: TEST_PAYLOAD,
-};
+import {
+  TEST_CLOUD_EVENT,
+  TEST_CONTEXT,
+  TEST_PAYLOAD,
+  TEST_PLUGIN_OPTIONS,
+} from '../data/test_data/async_plugin';
 
 describe('OpenFunction - Async - Binding with plugin', () => {
   const APPID = 'async.dapr';
